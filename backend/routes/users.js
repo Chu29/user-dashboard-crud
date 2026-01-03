061d0fb734cffd1dca717e5a4cd6d567f4ba1c08
Chu A. <chu.amk22@gmail.com>
2026-01-03 10:56:46 +0100

    implement api route to get a user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,3 +11,14 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// Get single user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findByID(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
