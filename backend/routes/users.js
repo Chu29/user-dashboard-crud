3f914bf5bce75f3f4f3ff5d79d9b1b615e278873
Chu A. <chu.amk22@gmail.com>
2026-01-03 10:59:35 +0100

    implement api route to create a user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -22,3 +22,19 @@ router.get("/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// Create user
+router.post("/", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+    role: req.body.role,
+  });
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
