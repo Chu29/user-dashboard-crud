5e2ee4d8b403b007561f143af031d0ae8913102e
Chu A. <chu.amk22@gmail.com>
2026-01-03 10:54:11 +0100

    implement api route to get all users

---
+++ backend/routes/users.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+const User = require("../models/User");
+
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().sort({ createdAt: -1 });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
